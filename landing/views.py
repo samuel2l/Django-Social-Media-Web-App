03f8e04641889ac4794b8dfca6bde2fb5b516276
Samuel Adams <sama29571@gmail.com>
2024-09-18 00:59:12 +0000

    add templates

--- landing/views.py
+++ landing/views.py
@@ -5,4 +5,4 @@ from django.views import View
 
 class Index(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'landing/index.html')
+        return render(request, 'index.html')
