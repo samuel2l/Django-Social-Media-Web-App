aab8a2ff38b6b20f164f54871e0361c096231545
Samuel Adams <sama29571@gmail.com>
2024-09-20 07:55:43 +0000

    add view for posts

--- social/views.py
+++ social/views.py
@@ -5,6 +5,8 @@ from .models import Post
 # Create your views here.
 class HomeView(ListView):
     model=Post
-    template_name='home.html'
+    template_name='posts.html'
     context_object_name='posts'#defines the name of the context you are sending to your template. by default it is called object
     ordering=['-date_created'] #field on which to order results. -date means date in descending order
+
+
