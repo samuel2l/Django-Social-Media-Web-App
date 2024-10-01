1b8a981b0c31d7877e8b1fa446fd6737ed9fcd14
Samuel Adams <sama29571@gmail.com>
2024-10-01 22:56:15 +0000

    add notifications custom tag

--- landing/templatetags/custom_tags.py
+++ landing/templatetags/custom_tags.py
@@ -5,8 +5,8 @@ register = template.Library()
 
 @register.inclusion_tag('show_notifications.html', takes_context=True)
 def show_notifications(context):
-	request_user = context['request'].user
-	notifications = Notification.objects.filter(to_user=request_user).exclude(user_has_seen=True).order_by('-date')
+	current_user = context['request'].user
+	notifications = Notification.objects.filter(receiver=current_user).exclude(seen=True).order_by('-date')
 	return {'notifications': notifications}
 
 
